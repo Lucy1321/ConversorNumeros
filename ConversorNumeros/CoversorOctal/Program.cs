0d993a8c1a9ca3975c5c1f417137fc1af2da1ccf
unknown <lrgordian@gmail.com>
2018-07-23 21:03:33 -0400

    todo

---
+++ ConversorNumeros/CoversorOctal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoversorOctal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
